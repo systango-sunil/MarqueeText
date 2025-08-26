f0d1529bea2cab9b4077e10b9934a7be3bb73f39
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-08-26 19:20:51 +0530

    Version issue fixed

--- Sources/MarqueeText/SwiftUI+Extension.swift
+++ Sources/MarqueeText/SwiftUI+Extension.swift
@@ -12,13 +12,13 @@ import Combine
 
 extension View {
     /// A backwards compatible wrapper for iOS 14 `onChange`
-    @ViewBuilder func onValueChanged<T: Equatable>(of value: T, perform onChange: @escaping (T) -> Void) -> some View {
-        if #available(iOS 14.0, *) {
-            self.onChange(of: value, perform: onChange)
-        } else {
-            self.onReceive(Just(value)) { (value) in
-                onChange(value)
-            }
-        }
-    }
+//    @ViewBuilder func onValueChanged<T: Equatable>(of value: T, perform onChange: @escaping (T) -> Void) -> some View {
+//        if #available(iOS 14.0, *) {
+//            self.onChange(of: value, perform: onChange)
+//        } else {
+//            self.onReceive(Just(value)) { (value) in
+//                onChange(value)
+//            }
+//        }
+//    }
 }
