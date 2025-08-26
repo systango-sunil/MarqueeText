210f7f5b9273845bcbd98a4e73db097d2a4c9697
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-08-26 19:31:14 +0530

    Version issue fixed

--- Sources/MarqueeText/MarqueeText.swift
+++ Sources/MarqueeText/MarqueeText.swift
@@ -58,7 +58,7 @@ public struct MarqueeText: View {
                     // MARK: - Non-scrolling version
                     Text(text)
                         .font(.init(font))
-                        .onChange(of: text, initial: true) { oldValue, newValue in
+                        .onValueChanged(of: text, initial: true) { oldValue, newValue in
                             self.animate = false
                         }
                         .frame(
@@ -74,7 +74,7 @@ public struct MarqueeText: View {
                 // Trigger scrolling if needed
                 self.animate = needsScrolling
             }
-            .onChange(of: text, initial: true) { oldValue, newValue in
+            .onValueChanged(of: text, initial: true) { oldValue, newValue in
                 let newStringWidth = newValue.widthOfString(usingFont: font)
                 if newStringWidth > geo.size.width {
                     // Stop the old animation first
@@ -88,6 +88,7 @@ public struct MarqueeText: View {
                     self.animate = false
                 }
             }
+
         }
         .frame(height: stringHeight)
         .frame(maxWidth: isCompact ? stringWidth : nil)
--- Sources/MarqueeText/SwiftUI+Extension.swift
+++ Sources/MarqueeText/SwiftUI+Extension.swift
@@ -11,14 +11,22 @@ import SwiftUI
 import Combine
 
 extension View {
-    /// A backwards compatible wrapper for iOS 14 `onChange`
-//    @ViewBuilder func onValueChanged<T: Equatable>(of value: T, perform onChange: @escaping (T) -> Void) -> some View {
-//        if #available(iOS 14.0, *) {
-//            self.onChange(of: value, perform: onChange)
-//        } else {
-//            self.onReceive(Just(value)) { (value) in
-//                onChange(value)
-//            }
-//        }
-//    }
+    /// A backwards compatible wrapper for iOS 14+ `onChange(of:)` and iOS 17+ `onChange(of:initial:)`
+    @ViewBuilder func onValueChanged<T: Equatable>(
+        of value: T,
+        initial: Bool = false,
+        perform action: @escaping (_ oldValue: T, _ newValue: T) -> Void
+    ) -> some View {
+        if #available(iOS 17.0, *) {
+            self.onChange(of: value, initial: initial, perform: action)
+        } else if #available(iOS 14.0, *) {
+            self.onChange(of: value) { newValue in
+                action(value, newValue) // ⚠️ oldValue isn’t available pre-iOS 17
+            }
+        } else {
+            self.onReceive(Just(value)) { newValue in
+                action(value, newValue) // again, oldValue isn’t tracked here
+            }
+        }
+    }
 }
