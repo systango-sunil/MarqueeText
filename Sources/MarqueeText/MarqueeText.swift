ba770f14c01915d22630254af7a3d8f7c14f1e49
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-08-26 19:26:00 +0530

    Version issue fixed

--- Sources/MarqueeText/MarqueeText.swift
+++ Sources/MarqueeText/MarqueeText.swift
@@ -58,7 +58,7 @@ public struct MarqueeText: View {
                     // MARK: - Non-scrolling version
                     Text(text)
                         .font(.init(font))
-                        .onChange(of: text) { _ in
+                        .onChange(of: text, initial: text) { oldValue, newValue in
                             self.animate = false
                         }
                         .frame(
@@ -74,7 +74,7 @@ public struct MarqueeText: View {
                 // Trigger scrolling if needed
                 self.animate = needsScrolling
             }
-            .onChange(of: text) { newValue in
+            .onChange(of: text, initial: text) { oldValue, newValue in
                 let newStringWidth = newValue.widthOfString(usingFont: font)
                 if newStringWidth > geo.size.width {
                     // Stop the old animation first
