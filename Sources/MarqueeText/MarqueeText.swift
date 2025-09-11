59bbabf1c3de53f5d0d46dc8c96941520c46a7bc
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-09-12 02:09:50 +0530

    Sliding speed issue

--- Sources/MarqueeText/MarqueeText.swift
+++ Sources/MarqueeText/MarqueeText.swift
@@ -18,7 +18,8 @@ public struct MarqueeText: View {
         
         GeometryReader { geo in
             let needsScrolling = stringWidth > geo.size.width
-            let travelDistance = stringWidth
+            let totalWidth = stringWidth + 20 // width of one text + spacing
+            let travelDistance = totalWidth
             let duration = travelDistance / pointsPerSecond
             
             ZStack {
