64dea282057c54d73fb19778c49fd29c55f742b7
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-09-12 01:21:49 +0530

    Sliding speed issue

--- Sources/MarqueeText/MarqueeText.swift
+++ Sources/MarqueeText/MarqueeText.swift
@@ -6,110 +6,58 @@ public struct MarqueeText: View {
     public var leftFade: CGFloat
     public var rightFade: CGFloat
     public var startDelay: Double
-    public var alignment: Alignment
     public var pointsPerSecond: Double
     
     @State private var animate = false
-    var isCompact = false
     
     public var body: some View {
-        let stringWidth  = text.widthOfString(usingFont: font)
+        let stringWidth = text.widthOfString(usingFont: font)
         let stringHeight = text.heightOfString(usingFont: font)
         
         GeometryReader { geo in
-            let needsScrolling = (stringWidth > geo.size.width)
+            let needsScrolling = stringWidth > geo.size.width
+            let travelDistance = stringWidth + geo.size.width
             
             ZStack {
                 if needsScrolling {
-                    // total distance the text needs to travel
-                    let travelDistance = stringWidth + geo.size.width + 40
-                    
-                    let animation = Animation
-                        .linear(duration: travelDistance / pointsPerSecond)
-                        .delay(startDelay)
-                        .repeatForever(autoreverses: false)
-                    
-                    let nullAnimation = Animation.linear(duration: 0)
-                    
-                    makeMarqueeTexts(
-                        travelDistance: travelDistance,
-                        animation: animation,
-                        nullAnimation: nullAnimation
+                    HStack(spacing: 40) { // small gap between repetitions
+                        Text(text)
+                            .font(.init(font))
+                            .lineLimit(1)
+                            .fixedSize()
+                        
+                        Text(text)
+                            .font(.init(font))
+                            .lineLimit(1)
+                            .fixedSize()
+                    }
+                    .offset(x: animate ? -travelDistance : 0)
+                    .animation(
+                        animate ?
+                            Animation.linear(duration: travelDistance / pointsPerSecond)
+                                .delay(startDelay)
+                                .repeatForever(autoreverses: false)
+                            : .default,
+                        value: animate
                     )
-                    .frame(
-                        minWidth: 0,
-                        maxWidth: .infinity,
-                        minHeight: 0,
-                        maxHeight: .infinity,
-                        alignment: .topLeading
-                    )
-                    .offset(x: leftFade)
                     .mask(
-                        fadeMask(
-                            leftFade: leftFade,
-                            rightFade: rightFade
-                        )
+                        fadeMask(leftFade: leftFade, rightFade: rightFade)
                     )
-                    .frame(width: geo.size.width + leftFade)
-                    .offset(x: -leftFade)
                 } else {
-                    // Non-scrolling case
                     Text(text)
                         .font(.init(font))
-                        .frame(
-                            minWidth: 0,
-                            maxWidth: .infinity,
-                            minHeight: 0,
-                            maxHeight: .infinity,
-                            alignment: alignment
-                        )
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
             }
-            .onAppear {
-                self.animate = needsScrolling
-            }
-            .onValueChanged(of: text, initial: true) { _, newValue in
-                let newStringWidth = newValue.widthOfString(usingFont: font)
-                self.animate = newStringWidth > geo.size.width
-            }
+            .onAppear { animate = needsScrolling }
+            .onDisappear { animate = false }
         }
         .frame(height: stringHeight)
-        .frame(maxWidth: isCompact ? stringWidth : nil)
-        .onDisappear {
-            self.animate = false
-        }
-    }
-    
-    // MARK: - Marquee pair of texts
-    @ViewBuilder
-    private func makeMarqueeTexts(
-        travelDistance: CGFloat,
-        animation: Animation,
-        nullAnimation: Animation
-    ) -> some View {
-        Group {
-            Text(text)
-                .lineLimit(1)
-                .font(.init(font))
-                .offset(x: animate ? -travelDistance : 0)
-                .animation(animate ? animation : nullAnimation, value: animate)
-                .fixedSize(horizontal: true, vertical: false)
-            
-            Text(text)
-                .lineLimit(1)
-                .font(.init(font))
-                .offset(x: animate ? 0 : travelDistance)
-                .animation(animate ? animation : nullAnimation, value: animate)
-                .fixedSize(horizontal: true, vertical: false)
-        }
     }
     
     // MARK: - Fade mask
-    @ViewBuilder
     private func fadeMask(leftFade: CGFloat, rightFade: CGFloat) -> some View {
         HStack(spacing: 0) {
-            Rectangle().frame(width: 2).opacity(0)
-            
             LinearGradient(
                 gradient: Gradient(colors: [Color.black.opacity(0), Color.black]),
                 startPoint: .leading,
@@ -117,11 +65,7 @@ public struct MarqueeText: View {
             )
             .frame(width: leftFade)
             
-            LinearGradient(
-                gradient: Gradient(colors: [Color.black, Color.black]),
-                startPoint: .leading,
-                endPoint: .trailing
-            )
+            Color.black
             
             LinearGradient(
                 gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]),
@@ -129,8 +73,6 @@ public struct MarqueeText: View {
                 endPoint: .trailing
             )
             .frame(width: rightFade)
-            
-            Rectangle().frame(width: 2).opacity(0)
         }
     }
     
@@ -141,37 +83,25 @@ public struct MarqueeText: View {
         leftFade: CGFloat = 16,
         rightFade: CGFloat = 16,
         startDelay: Double = 1,
-        alignment: Alignment? = nil,
         pointsPerSecond: Double = 30
     ) {
-        self.text      = text
-        self.font      = font
-        self.leftFade  = leftFade
+        self.text = text
+        self.font = font
+        self.leftFade = leftFade
         self.rightFade = rightFade
         self.startDelay = startDelay
-        self.alignment = alignment ?? .topLeading
         self.pointsPerSecond = pointsPerSecond
-    }
-}
-
-extension MarqueeText {
-    public func makeCompact(_ compact: Bool = true) -> Self {
-        var view = self
-        view.isCompact = compact
-        return view
     }
 }
 
 extension String {
     func widthOfString(usingFont font: UIFont) -> CGFloat {
         let fontAttributes = [NSAttributedString.Key.font: font]
-        let size = self.size(withAttributes: fontAttributes)
-        return size.width
+        return self.size(withAttributes: fontAttributes).width
     }
     
     func heightOfString(usingFont font: UIFont) -> CGFloat {
         let fontAttributes = [NSAttributedString.Key.font: font]
-        let size = self.size(withAttributes: fontAttributes)
-        return size.height
+        return self.size(withAttributes: fontAttributes).height
     }
 }
